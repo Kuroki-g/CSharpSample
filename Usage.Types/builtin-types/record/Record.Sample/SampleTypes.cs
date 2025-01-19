e6006e401346405129425f65cf67375ca9472b24
Kuroki-g <64629990+Kuroki-g@users.noreply.github.com>
2025-01-19 21:17:31 +0900

    Add Makefile and sample types for Record.Sample project; update .gitignore

---
+++ Usage.Types/builtin-types/record/Record.Sample/SampleTypes.cs
@@ -0,0 +1,46 @@
+namespace Usage.Types.Record.Sample;
+
+public class SampleClass(int x, int y)
+{
+    public int X { get; set; } = x;
+
+    public int Y { get; set; } = y;
+
+    public override string ToString() => $"X: {X}, Y: {Y}";
+}
+
+public struct SampleStruct(int x, int y)
+{
+    public int X { get; set; } = x;
+    public int Y { get; set; } = y;
+
+    public override string ToString() => $"X: {X}, Y: {Y}";
+}
+
+public readonly struct SampleReadonlyStruct(int x, int y)
+{
+    // public int X { get; set; } = x; // Error: Property or indexer cannot be assigned to -- it is read only
+    public int X { get; init; } = x;
+
+    public int Y { get; init; } = y;
+
+    public override string ToString() => $"X: {X}, Y: {Y}";
+}
+
+public ref struct SampleStructRef(int x, int y)
+{
+    public int X { get; set; } = x;
+    public int Y { get; set; } = y;
+
+    public override string ToString() => $"X: {X}, Y: {Y}";
+}
+
+public readonly ref struct SampleReadonlyStructRef(int x, int y)
+{
+    // public int X { get; set; } = x; // Error: Property or indexer cannot be assigned to -- it is read only
+    public int X { get; init; } = x;
+
+    public int Y { get; init; } = y;
+
+    public override string ToString() => $"X: {X}, Y: {Y}";
+}
